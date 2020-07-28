69a79b50ca64cdeabd42b29c9599253ecfb44f43
YUKIJUDAI <1787149082@qq.com>
2020-07-28 12:01:45 +0800

    init

--- src/components/head.vue
+++ src/components/head.vue
@@ -18,22 +18,24 @@
                 <div class="title">
                     <img src="~@/assets/img/title5.png">
                 </div>
-                <div class="search">
+                <div class="search" @click="searchFlag = true">
                     <img src="~@/assets/img/search.png">
                 </div>
             </div>
-            <div class="search">
-                <div class="search-input">
-                    <input type="text" />
-                    <i class="icon-search"></i>
+            <transition name="fade">
+                <div class="search" v-if="searchFlag">
+                    <div class="search-input">
+                        <input type="text" v-model="searchText" />
+                        <i class="icon-search"></i>
+                    </div>
+                    <div class="line">
+                        <i class="icon-line"></i>
+                    </div>
+                    <div class="close">
+                        <i class="icon-close" @click="close"></i>
+                    </div>
                 </div>
-                <div class="line">
-                    <i class="icon-line"></i>
-                </div>
-                <div class="close">
-                    <i class="icon-close"></i>
-                </div>
-            </div>
+            </transition>
         </main>
     </div>
 </template>
@@ -43,7 +45,20 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Head extends Vue {
+    searchText: string = "";
+    searchFlag: boolean = false;
 
+    mounted() {
+        document.onkeydown = (e) => {
+            if (!this.searchFlag || this.searchText === "") return;
+            if (e && e.keyCode === 13) this.$router.push("/serach?key=" + this.searchText);
+        }
+    }
+
+    close() {
+        this.searchText = "";
+        this.searchFlag = false;
+    }
 }
 </script>
 
@@ -55,6 +70,7 @@ export default class Head extends Vue {
     position: fixed;
     top: 0;
     left: 0;
+    z-index: 99;
     .head-main {
         display: flex;
         align-items: center;
@@ -86,12 +102,13 @@ export default class Head extends Vue {
         }
         .icon-search {
             position: absolute;
-            top: 5px;
+            top: 0px;
             right: 0px;
             width: 29px;
             height: 29px;
             display: block;
             background: url("~@/assets/img/icon-search.png");
+            cursor: pointer;
         }
     }
     .line {
@@ -105,14 +122,31 @@ export default class Head extends Vue {
     }
     .close {
         width: 100%;
-        
         .icon-close {
             background: url("~@/assets/img/icon-close.png");
             display: block;
             width: 47px;
             height: 47px;
-            margin: 0 auto;
+            margin: 35px auto;
+            cursor: pointer;
+            transition: All 0.2s ease-in-out;
+            -webkit-transition: All 0.2s ease-in-out;
+            -moz-transition: All 0.2s ease-in-out;
+            -o-transition: All 0.2s ease-in-out;
+            &:hover {
+                transform: rotate(90deg);
+            }
         }
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+    .fade-leave-active {
+        transition: opacity 0.3s;
+    }
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
     }
 }
 </style>
