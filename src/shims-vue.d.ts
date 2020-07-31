fa8c2c2d1c4665ec3f6dbb634a05a6f3669c9244
YUKIJUDAI <1787149082@qq.com>
2020-07-31 16:28:21 +0800

    init

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,10 @@
 import { Store } from "vuex/types/index";
 
+declare module "*.vue" {
+    import Vue from "vue";
+    export default Vue;
+}
+
 declare module "vue/types/vue" {
     interface Vue {
         $http: any;
